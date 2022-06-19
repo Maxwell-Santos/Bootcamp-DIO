c7120b88fe59d29e1dcaf0743589b35edd19fc0d
Maxwell <max.coding011@gmail.com>
2022-06-19 14:51:50 -0300

    ajuste loading

--- Impulso Javascript Evolution/Desafio de Projeto/Página inicial do Instagram/assets/loader/preLoad.js
+++ Impulso Javascript Evolution/Desafio de Projeto/Página inicial do Instagram/assets/loader/preLoad.js
@@ -1,7 +1,13 @@
-const preload = document.querySelector('.preload')
 
 window.addEventListener('load', () => {
-  const preloader = document.querySelector(".preload")
-  preloader.classList.add('finish')
+  setTimeout(() => {
+
+    const preloader = document.querySelector('.preload')
+
+    preloader.classList.add('finish');
+    preloader.classList.remove('false');
+
+
+  }, 1000);
 
 });
