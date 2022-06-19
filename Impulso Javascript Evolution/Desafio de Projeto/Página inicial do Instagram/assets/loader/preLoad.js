ebcfc0168dbc5cd83c36e2ad04875c7db99a6243
Maxwell <max.coding011@gmail.com>
2022-06-19 15:01:35 -0300

    ajuste tela de loading

--- Impulso Javascript Evolution/Desafio de Projeto/Página inicial do Instagram/assets/loader/preLoad.js
+++ Impulso Javascript Evolution/Desafio de Projeto/Página inicial do Instagram/assets/loader/preLoad.js
@@ -1,12 +1,11 @@
 
 window.addEventListener('load', () => {
+  
   setTimeout(() => {
-
     const preloader = document.querySelector('.preload')
 
     preloader.classList.add('finish');
     preloader.classList.remove('false');
-
 
   }, 1000);
 
